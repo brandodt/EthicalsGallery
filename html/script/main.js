30288a086225377813fd4815ac59714d7b5dd46b
Dwayne Lifter <67250900+dwayne-lifter@users.noreply.github.com>
2023-06-26 02:38:02 +0800

    more animation

--- html/script/main.js
+++ html/script/main.js
@@ -54,3 +54,19 @@ document.addEventListener('DOMContentLoaded', function () {
         return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
     }
 });
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
